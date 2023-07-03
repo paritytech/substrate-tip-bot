3b0c2259b6e502942680cc8d530c4912d981be3e
rzadp <roopert7@gmail.com>
2023-07-03 15:17:06 +0200

    Include all

--- src/util.ts
+++ src/util.ts
@@ -132,4 +132,4 @@ export const formatTipSize = (tipRequest: TipRequest): string => {
  * Currently - Engineering Automation / Opstooling.
  * It is used to tag these usernames when there is a failure.
  */
-export const teamMatrixHandles = ["@przemek"];
+export const teamMatrixHandles = ["@przemek", "@mak", "@yuri", "@bullrich"];
