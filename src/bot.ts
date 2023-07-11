c85217c97edaaa9b96d1ef6ea761e8d72736c23d
Przemek Rzad <przemek@parity.io>
2023-07-11 13:15:47 +0200

    Read the contributor address from their github profile (bio) (#102)

--- src/bot.ts
+++ src/bot.ts
@@ -73,7 +73,8 @@ const onIssueComment = async (
 
   let contributorAccount: ContributorAccount;
   try {
-    contributorAccount = parseContributorAccount(pullRequestBody);
+    const userData = await octokitInstance.rest.users.getByUsername({ username: contributorLogin });
+    contributorAccount = parseContributorAccount([pullRequestBody, userData.data.bio]);
   } catch (error: unknown) {
     return { type: "error", errorMessage: `@${contributorLogin} ${(error as Error).message}` };
   }
